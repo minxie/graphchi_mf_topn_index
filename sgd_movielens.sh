74ff694c9c696e9076cd0ea8aaebbeb66002d860
Yidan Liu <yidanliu@champagne.cs.ubc.ca>
2013-11-14 22:18:09 -0800

    convergence threshold added

--- sgd_movielens.sh
+++ sgd_movielens.sh
@@ -3,7 +3,7 @@ for m in '50'
 do
 	for l in  '1e-3'
   	do
-		../bin/myapps/sgd --D=$m --training=../../data/ml-10M100K/ratings.dat_mm --result=../../result/gc_movielens_res\
+		../bin/myapps/sgd --D=$m --training=../../data/ml-10M100K/ratings.dat_mm --result=../../result/gc_movielens_res \
 		  --sgd_gamma=1e-4 --sgd_lambda=1e-4 --max_iter=100 --n_top=50 --quiet=1 --tol=1e-3
     	done
 done
--- sgd_yahoomusic.sh
+++ sgd_yahoomusic.sh
@@ -3,7 +3,7 @@ for m in '50'
 do
 	for l in  'le-3'
 	do
-		../bin/myapps/sgd --D=$m --training=../../data/yahoomusic/ratings_kddcup_mm --tol= 1e-1 --result=../../result/gc_yahoomusic_res\
+		../bin/myapps/sgd --D=$m --training=../../data/yahoomusic/ratings_kddcup_mm --tol= 1e-1 --result=../../result/gc_yahoomusic_res \
 		  --sgd_gamma=1e-4 --sgd_lambda=1e-4 --max_iter=100 --n_top=50 --quiet=1  --ff=4 --tol=1e-1
 	done
 done
